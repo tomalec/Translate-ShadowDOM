96695819c5753655a20f5bc1b78428741c79e774
Tomek Wytrebowicz <tomalecpub@gmail.com>
2017-02-21 17:37:48 +0100

    v0.0.2

--- translate-shadowdom.js
+++ translate-shadowdom.js
@@ -3,7 +3,7 @@
  * Usefull when writing "hybrid" Web Components which are V1 redy,
  * but run in v0 environment/polyfill
  * @license MIT
- * @version: 0.0.1
+ * @version: 0.0.2
  */
 const TranslateShadowDOM = {
     /**
